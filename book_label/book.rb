a88fdada7d277c08303032f2756dd1c3c1de28f7
ecekpo <ememekpo@yahoo.com>
2023-01-09 23:12:21 +0100

    Add created can_be_archived? method in Book class

--- book_label/book.rb
+++ book_label/book.rb
@@ -12,4 +12,11 @@ end
 def can_be_archived?
     super || cover_state == 'bad'
   end
+
+  def create_json
+    super.merge({
+      publisher: @publisher,
+      cover_state; @cover_state
+      })
+    end
 end
