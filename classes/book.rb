d645c2d871b395f42968b7870a834c8c13ca7bb2
ecekpo <ememekpo@yahoo.com>
2023-01-11 11:12:32 +0100

    fix linter errors

--- classes/book.rb
+++ classes/book.rb
@@ -7,9 +7,9 @@ class Book < Item
     super(published_date, false, id)
     @publisher = publisher
     @cover_state = cover_state
-end
+  end
 
-def can_be_archived?
+  def can_be_archived?
     super || cover_state == 'bad'
   end
 end
