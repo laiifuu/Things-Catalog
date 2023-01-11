4823490b0ffe6f906ccffc559f57187864b55c5e
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-01-11 05:45:09 +0100

    Fix merge conflict

--- main.rb
+++ main.rb
@@ -49,21 +49,3 @@ end
 
 main = Main.new
 main.run
-# def run
-#   puts 'Welcome to your catalog of things!'
-#   exit = false
-#   until exit
-#     show_options
-#     number = gets.to_i
-#     if number < 11
-#       puts "Number chosen #{number}"
-#     elsif number == 11
-#       exit = true
-#       puts 'Thanks for using our catalog of things app, see you soon!'
-#     else
-#       puts "Error friend, wrong input. Sorry I don't make the rules ¯\\(ツ)/¯"
-#     end
-#   end
-# end
-
-# run
